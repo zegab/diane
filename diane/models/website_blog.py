797d3722f32cdbea1dcba9a9c8296d2946f43848
zegab <email@gabriel-demmerle.com>
2020-11-24 19:44:15 +0100

    Perspectives: add fields on blog model

--- diane/models/website_blog.py
+++ diane/models/website_blog.py
@@ -11,3 +11,17 @@ class BlogPost(models.Model):
 	_inherit = 'blog.post'
 
 	sequence = fields.Integer('Sequence')
+
+	#perspectives: add related fields from the partner to display the videos + interview
+	alumni_id = fields.Many2one('res.partner', 'Alumni')
+	video_url = fields.Char('Video URL')
+
+	forename = fields.Char(related='alumni_id.forename', string='Forename', readonly=True)
+	lastname = fields.Char(related='alumni_id.lastname', string='Last Name', readonly=True)
+	x_title = fields.Char(related='alumni_id.x_title', string='Title', readonly=True)
+	diploma = fields.Many2one(related='alumni_id.diploma', string='Diploma', readonly=True)
+	section = fields.Many2one(related='alumni_id.section', string='Section', readonly=True)
+	d_year = fields.Integer(related='alumni_id.d_year', string='Diploma Year', readonly=True)
+	d_other = fields.Char(related='alumni_id.d_other', string='Other Diplomas', readonly=True)
+	function = fields.Char(related='alumni_id.function', string='Company Name', readonly=True)
+	c_name = fields.Char(related='alumni_id.c_name', string='Company Name', readonly=True)
