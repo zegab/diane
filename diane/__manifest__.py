4a78543891fcb1cf0109db3494f65e234b08e934
gab-devadoo <email@gabriel-demmerle.com>
2022-10-21 17:30:11 +0200

    [MIG] alumni_search inline form

--- diane/__manifest__.py
+++ diane/__manifest__.py
@@ -33,7 +33,7 @@
 #        'views/alumni_message.xml',
         'views/alumni_map.xml',
 #        'views/website_jobs.xml',
-        'views/hr_recruitment.xml',
+#        'views/hr_recruitment.xml',
 #        'views/signup_template.xml',
          'security/ir.model.access.csv',
     ],
